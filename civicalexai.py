4c15fe4111062cbd874b51666aff5c1acef18ec1
uttam-bc <uttambc03@gmail.com>
2025-11-26 15:16:43 +0530

    abc

---
+++ civicalexai.py
@@ -0,0 +1,84 @@
+# civicalex ai bot
+#comment
+#import google api 
+import os
+
+
+try:
+    GOOGLE_API_KEY = AIzaSyCsc791_A3zX4oPGGpjL5q3xksFxZAjQbo
+    os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+    print("✅ Gemini API key setup complete.")
+except Exception as e:
+    print(
+        f"🔑 Authentication Error: Please make sure you have added 'GOOGLE_API_KEY' to your Kaggle secrets. Details: {e}"
+    )
+    
+#import adk components
+from google.adk.agents import Agent, SequentialAgent, ParallelAgent, LoopAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.genai import types
+
+print("✅ ADK components imported successfully.")
+
+#configure retry options
+retry_config=types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
+)
+
+#multi agent system
+
+#agent1
+research_agent = Agent(
+    name="ResearchAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ), #provide instruction
+    instruction="""You are a specialized research agent. Your only job is to use the 
+    google_search tool to find 2-3 pieces of relevant information on the given topic and present the findings with citations.""",
+    tools=[google_search],
+    output_key="research_findings",  # The result of this agent will be stored in the session state with this key.
+)
+
+#agent2
+summarizer_agent = Agent(
+    name="SummarizerAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    # The instruction is modified to request a bulleted list for a clear output format.
+    instruction="""Read the provided research findings: {research_findings}
+Create a concise summary as a bulleted list with 3-5 key points.""",
+    output_key="final_summary",
+)
+
+
+#root coordinator / agent
+root_agent = Agent(
+    name="ResearchCoordinator",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    # This instruction tells the root agent HOW to use its tools (which are the other agents).
+    instruction="""You are a research coordinator. Your goal is to answer the user's query by orchestrating a workflow.
+1. First, you MUST call the `ResearchAgent` tool to find relevant information on the topic provided by the user.
+2. Next, after receiving the research findings, you MUST call the `SummarizerAgent` tool to create a concise summary.
+3. Finally, present the final summary clearly to the user as your response.""",
+    # We wrap the sub-agents in `AgentTool` to make them callable tools for the root agent.
+    tools=[AgentTool(research_agent), AgentTool(summarizer_agent)],
+)
+
+print("✅ root_agent created.")
+
+#run the script
+runner = InMemoryRunner(agent=root_agent)
+response = await runner.run_debug(
+    "What are the latest advancements in quantum computing and what do they mean for AI?"
+)
